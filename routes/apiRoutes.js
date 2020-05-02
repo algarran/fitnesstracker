db503fa064e4a4646bd8c84ede357392b77221ad
Nick Algarra <algarran24@gmail.com>
2020-05-02 11:54:11 -0500

    updated code with development files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,61 @@
+//require the express router
+const router = require('express').Router();
+
+//using the workout model
+const Workout = require('../models/workouts.js');
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+    .then((result) => {
+        res.json(result);
+    })
+    .catch((err) => {
+        res.status(404).json(err)
+    });
+});
+
+router.get('/api/workouts', (req, res) => {
+    Workout.find({})
+    .sort({ date: -1 })
+    .then((result) => {
+        result.forEach((workout) => {
+            let total = 0;
+            workout.exercies.forEach((exercie) => {
+             total += exercie.duration;
+            });
+        workout.totalDuration = total;
+        });
+        res.json(result);
+    })
+    .catch((err) => {
+        res.status(404).json(err);
+    });
+});
+
+router.get('/api/workouts/range', (req, res) => {
+    Workout.find({})
+    .sort({ data: -1 })
+    .then((result) => {
+        res.json(result);
+    })
+    .catch((err) => {
+        res.status(404).json(err);
+    });
+});
+
+//add a workout to the plan
+router.put('/api/workouts/:id', (req, res) => {
+    Workout.updateOne({ _id: req.params.id }, {
+        $push: {
+            exercies: req.body,
+        },
+    })
+    .then((result) => {
+        res.json(result);
+    })
+    .catch((err) => {
+        res.status(404).json(err);
+    });
+});
+
+module.exports = router;
